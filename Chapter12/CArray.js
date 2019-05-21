cd6dd90359d3c2639e5c30d35b499e0b0b797837
Chasey <songxb792@163.com>
2019-05-21 10:45:58 +0800

    add function qSort mergeSort shellSort

--- Chapter12/CArray.js
+++ Chapter12/CArray.js
@@ -20,6 +20,14 @@ function CArray(numElements){
 	
 	//插入排序
 	this.insertSort = insertSort;
+	
+	//希尔排序
+	this.gaps = [5,3,1];
+	this.shellSort = shellSort;
+	
+	//归并排序
+	this.mergeSort = mergeSort;
+	this.mergeArrays = mergeArrays;
 }
 
 function setData(){
@@ -104,3 +112,72 @@ function insertSort(){
 }
 
 //希尔排序
+function shellSort(){
+	for(var g = 0; g < this.gaps.length; g++){
+		for(var i = this.gaps[g]; i < this.dataStore.length; i++){
+			var temp = this.dataStore[i];
+			for(var j = i; j >= this.gaps[g] && this.dataStore[j - this.gaps[g]] > temp; j -= this.gaps[g]){
+				this.dataStore[j] = this.dataStore[j - this.gaps[g]];
+			}
+			
+			this.dataStore[j] = temp;
+		}
+	}
+}
+
+//归并排序
+function mergeSort(){
+	if(this.dataStore.length < 2){
+		return;
+	}
+	
+	var step = 1;
+	var left,right;
+	while(step <  this.dataStore.length){
+		left = 0;
+		right = step;
+		while(right + step <= this.dataStore.length){
+			mergeArrays(this.dataStore,left,left+step,right,right+step);
+			left = right + step;
+			right = left + step;
+		}
+		if(right < this.dataStore.length){
+			mergeArrays(this.dataStore,left,left+step,right,this.dataStore.length);
+		}
+		
+		step *=2;
+	}
+}
+
+function mergeArrays(arr,startLeft,stopLeft,startRight,stopRight){
+	var rightArr = new Array(stopRight - startRight + 1);
+	var leftArr = new Array(stopLeft - startLeft + 1);
+	k = startRight;
+	for(var i = 0; i < (rightArr.length - 1); i++){
+		rightArr[i] = arr[k];
+		k++;
+	}
+	
+	k = startLeft;
+	
+	for(var i = 0; i < (leftArr.length -1); i++){
+		leftArr[i] = arr[k];
+		k++
+	}
+	
+	rightArr[rightArr.length - 1] = Infinity;
+	leftArr[leftArr.length - 1] = Infinity;
+	var m = 0;
+	var n = 0;
+	for(var k = startLeft; k < stopRight; k++){
+		if(leftArr[m] <= rightArr[n]){
+			arr[k] = leftArr[m];
+			m++
+		}else{
+			arr[k] = rightArr[n];
+			n++;
+		}
+	}
+}
+
+//快速排序
